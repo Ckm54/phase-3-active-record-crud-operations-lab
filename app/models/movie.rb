0d42949fc5878767e07120e6c45ec1900b1a11bc
Collins <mutaic84@gmail.com>
2022-06-16 20:51:24 +0300

    commit tree 076407b5e60cc2671870e74ffa76fc2126da53ad

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -31,4 +31,22 @@ class Movie < ActiveRecord::Base
     # binding.pry
     Movie.where('release_date > 2002')
   end
+
+  def update_with_attributes(attributes)
+    # binding.pry
+    self.update(attributes)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    movie = Movie.find(id)
+    movie.destroy
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
 end
